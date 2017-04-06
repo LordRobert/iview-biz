ef3190ba090737898c2c3638a0df224ca631394b
litor <547794529@qq.com>
2017-04-06 22:01:18 +0800

    add new fragment tab

---
+++ fragments/common_fragment_tab.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Tabs>
+      <Tab-pane :label="item.title" v-for="item in options.tabs">
+        <component :is="item.component" :options="item.options" :events="item.events"></component>
+      </Tab-pane>
+    </Tabs>
+  </div>
+</template>
+<script>
+  /***
+   * @options
+   * tabs {Array} [{component:'', options:{}, events:{}]
+   */
+
+  import fragmentMixin from 'iview-biz/mixins/fragmentMixin'
+  export default {
+    mixins: [fragmentMixin],
+  }
+</script>
