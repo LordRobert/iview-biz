466ddac93b81f0d2feae7bfec6ba81139d1f172f
litor <547794529@qq.com>
2017-05-03 19:11:09 +0800

    format code

--- components/vertical-table/verticalTable.vue
+++ components/vertical-table/verticalTable.vue
@@ -1,34 +1,34 @@
 <template>
-    <div>
-        <div class="wrap">
-            <Row v-for="(rowIndex, row) in rows">
-                <i-col span="4" v-for="(colIndex, col) in row.cols">
-                    <div v-for="meta in meta" :class="{'title':col._label}" class="item">
-                        <span v-if="col._label">{{meta.title}}</span>
-                        <template v-if="!col._empty">
-                            <div v-if="!col._label && meta.render">
-                                {{{meta.render(row.cols, col._index)}}}
-                            </div>
-                            <div v-else>
-                                {{col[meta.key]}}
-                            </div>
-                        </template>
-                        <span v-if="col._empty">-</span>
-                    </div>
-                </i-col>
-            </Row>
-        </div>
+  <div>
+    <div class="wrap">
+      <Row v-for="(rowIndex, row) in rows">
+        <i-col span="4" v-for="(colIndex, col) in row.cols">
+          <div v-for="meta in meta" :class="{'title':col._label}" class="item">
+            <span v-if="col._label">{{meta.title}}</span>
+            <template v-if="!col._empty">
+              <div v-if="!col._label && meta.render">
+                {{{meta.render(row.cols, col._index)}}}
+              </div>
+              <div v-else>
+                {{col[meta.key]}}
+              </div>
+            </template>
+            <span v-if="col._empty">-</span>
+          </div>
+        </i-col>
+      </Row>
     </div>
+  </div>
 </template>
 <script>
-    /***
-     * @props
-     *  meta: 纵向表头信息
-     *  dataList: 数据
-     *
-     * @example
-     *
-     *  meta: [{
+  /***
+   * @props
+   *  meta: 纵向表头信息
+   *  dataList: 数据
+   *
+   * @example
+   *
+   *  meta: [{
           title: '科类',
           key: 'name'
         }, {
@@ -44,78 +44,78 @@
             return `  <Input-number :max="6" :min="0" :value.sync="dataList[${index}].wishNum" v-if="dataList[${index}].enable"></Input-number>`
           }
         }]
-     *
-     */
-    export default {
-        props:{
-            meta:Array,
-            dataList:Array
-        },
+   *
+   */
+  export default {
+    props: {
+      meta: Array,
+      dataList: Array
+    },
 
-        data: function () {
-            return {
-                rows: []
-            }
-        },
+    data: function () {
+      return {
+        rows: []
+      }
+    },
 
-        ready(){
-            this._format()
-        },
+    ready(){
+      this._format()
+    },
 
-        watch: {
-            'dataList': function () {
-                this._format()
-            }
-        },
+    watch: {
+      'dataList': function () {
+        this._format()
+      }
+    },
 
-        methods: {
-            _format(){
-                var length = this.dataList.length
-                var scoreTmp = []
-                this.dataList.forEach(function (item, index) {
-                    scoreTmp.push(Object.assign({_index: index}, _.cloneDeep(item)))
-                })
+    methods: {
+      _format(){
+        var length = this.dataList.length
+        var scoreTmp = []
+        this.dataList.forEach(function (item, index) {
+          scoreTmp.push(Object.assign({_index: index}, _.cloneDeep(item)))
+        })
 
-                var last = 5 - (length % 5 || 5)
+        var last = 5 - (length % 5 || 5)
 
-                for (var i = 0; i < last; i++) {
-                    scoreTmp.push({_empty: true})
-                }
-
-                var rowLength = scoreTmp.length / 5
-                var res = []
-
-
-                for (var i = 0; i < rowLength; i++) {
-                    var piece = _.slice(scoreTmp, i * 5, i * 5 + 5)
-                    res.push({cols: [{_label: true}].concat(piece)})
-                }
-
-                this.rows = res
-
-                this.$nextTick(function () {
-                    this.$compile(this.$el)
-                })
-            }
+        for (var i = 0; i < last; i++) {
+          scoreTmp.push({_empty: true})
         }
+
+        var rowLength = scoreTmp.length / 5
+        var res = []
+
+
+        for (var i = 0; i < rowLength; i++) {
+          var piece = _.slice(scoreTmp, i * 5, i * 5 + 5)
+          res.push({cols: [{_label: true}].concat(piece)})
+        }
+
+        this.rows = res
+
+        this.$nextTick(function () {
+          this.$compile(this.$el)
+        })
+      }
     }
+  }
 </script>
 <style scoped>
-    .wrap {
-        border-left: 1px solid rgba(216, 220, 240, 1);
-        border-top: 1px solid rgba(216, 220, 240, 1);
-    }
+  .wrap {
+    border-left: 1px solid rgba(216, 220, 240, 1);
+    border-top: 1px solid rgba(216, 220, 240, 1);
+  }
 
-    .item {
-        height: 40px;
-        line-height: 40px;
-        padding: 0 16px;
-        border-right: 1px solid rgba(216, 220, 240, 1);
-        border-bottom: 1px solid rgba(216, 220, 240, 1);
-    }
+  .item {
+    height: 40px;
+    line-height: 40px;
+    padding: 0 16px;
+    border-right: 1px solid rgba(216, 220, 240, 1);
+    border-bottom: 1px solid rgba(216, 220, 240, 1);
+  }
 
-    .title {
-        font-weight: 700;
-        background-color: #F4F6F8;
-    }
+  .title {
+    font-weight: 700;
+    background-color: #F4F6F8;
+  }
 </style>
--- fragments/common_fragment_table.vue
+++ fragments/common_fragment_table.vue
@@ -1,147 +1,151 @@
 <template>
-    <div>
-        <i-table :content="options.self" :data="options.tableData" :columns="options.tableColumns" stripe
-                 border @on-select="rowSelect" @on-selection-change="selectAll" :header-height="options.headerHeight"></i-table>
-        <div style="margin: 10px;overflow: hidden" v-if="options.pageable !== false">
-            <div style="float: right;">
-                <Page :total="total" :current="pageNumber" @on-change="onChange"
-                      @on-page-size-change="onPageSizeChange" :show-sizer="options.showSizer!==false" :page-size="pageSize" :page-size-opts="options.pageSizeOpts || [10, 20, 30, 40]" :show-elevator="options.showElevator!==false" :show-total="options.showTotal!==false"></Page>
-            </div>
-        </div>
+  <div>
+    <i-table :content="options.self" :data="options.tableData" :columns="options.tableColumns" stripe
+             border @on-select="rowSelect" @on-selection-change="selectAll"
+             :header-height="options.headerHeight"></i-table>
+    <div style="margin: 10px;overflow: hidden" v-if="options.pageable !== false">
+      <div style="float: right;">
+        <Page :total="total" :current="pageNumber" @on-change="onChange"
+              @on-page-size-change="onPageSizeChange" :show-sizer="options.showSizer!==false"
+              :page-size="pageSize" :page-size-opts="options.pageSizeOpts || [10, 20, 30, 40]"
+              :show-elevator="options.showElevator!==false" :show-total="options.showTotal!==false"></Page>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-    /**
-     *
-     * @options
-     *    tableData 表格数据
-     *    tableColumns 表格meata
-     *    pageable 是否有分页
-     *    url 请求数据url
-     *    pageSizeOpts 每页条数切换的配置
-     *    headerHeight 设置header高度
-     *    showElevator 是否显示跳转至 默认显示
-     *    showTotal 是否显示总条数
-     *    showSizer 是否展示分页条数
-     *
-     *
-     * @events
-     *    on-created vue created生命周期事件
-     *    on-after-ajax 当配置有url时  ajax返回后的事件
-     *    on-page-change 有分页时 切换分页时的事件
-     *    on-page-size-change 有分页时  切换每页展示条数时的事件
-     *    on-select 当配置有selection或singleSelection时 选中行的事件
-     *
-     */
-    import fragmentMixin from 'iview-biz/mixins/fragmentMixin'
-    export default {
-        mixins: [fragmentMixin],
-        mock: mockData(),
+  /**
+   *
+   * @options
+   *    tableData 表格数据
+   *    tableColumns 表格meata
+   *    pageable 是否有分页
+   *    url 请求数据url
+   *    pageSizeOpts 每页条数切换的配置
+   *    headerHeight 设置header高度
+   *    showElevator 是否显示跳转至 默认显示
+   *    showTotal 是否显示总条数
+   *    showSizer 是否展示分页条数
+   *    manualLoadData 是否手动获取数据 此时表格初始化时不执行ajax获取数据
+   *
+   *
+   * @events
+   *    on-created vue created生命周期事件
+   *    on-after-ajax 当配置有url时  ajax返回后的事件
+   *    on-page-change 有分页时 切换分页时的事件
+   *    on-page-size-change 有分页时  切换每页展示条数时的事件
+   *    on-select 当配置有selection或singleSelection时 选中行的事件
+   *
+   */
+  import fragmentMixin from 'iview-biz/mixins/fragmentMixin'
+  export default {
+    mixins: [fragmentMixin],
+    mock: mockData(),
 
-        props: {params: Object},
+    props: {params: Object},
 
-        data: function () {
-            return {
-                total: 0,
-                pageSize: 10,
-                pageNumber: 1
-            }
-        },
+    data: function () {
+      return {
+        total: 0,
+        pageSize: 10,
+        pageNumber: 1
+      }
+    },
 
-        ready(){
-            this.$emit('on-ready')
-            if (this.options.url) {
-                this._reload()
-            }
-        },
+    ready(){
+      this.$emit('on-ready')
+      if (this.options.url && this.options.manualLoadData !== true) {
+        this._reload()
+      }
+    },
 
-        methods: {
-            _reload(){
-                Utils.post(this.options.url, Object.assign({
-                    pageSize: this.pageSize,
-                    pageNumber: this.pageNumber
-                }, this.options.params)).then((res) => {
-                    this.total = res.datas.totalSize
-                    this.options.tableData = res.datas.rows
-                    this.$emit('on-after-ajax', res.datas)
-                })
-            },
+    methods: {
+      _reload(){
+        Utils.post(this.options.url, Object.assign({
+          pageSize: this.pageSize,
+          pageNumber: this.pageNumber
+        }, this.options.params)).then((res) => {
+          this.total = res.datas.totalSize
+          this.options.tableData = res.datas.rows
+          this.$emit('on-after-ajax', res.datas)
+        })
+      },
 
-            reload(){
-                this._reload()
-            },
+      reload(){
+        this._reload()
+      },
 
-            reloadFirstPage(){
-                this.pageNumber = 1
-                this.reload()
-            },
+      reloadFirstPage(){
+        this.pageNumber = 1
+        this.reload()
+      },
 
-            onChange(pageNumber){
-                this.pageNumber = pageNumber
-                this.$emit('on-page-change')
-                if (this.options.url) {
-                    this._reload()
-                }
-            },
-
-            onPageSizeChange(pageSize){
-                this.$emit('on-page-size-change')
-                this.pageSize = pageSize
-                if (this.options.url) {
-                    this._reload()
-                }
-            },
-
-            rowSelect(data){
-                this.$emit('on-select', data)
-            },
-
-            selectAll(){
-                this.$emit('on-selection-change', ...arguments)
-            }
+      onChange(pageNumber){
+        this.pageNumber = pageNumber
+        this.$emit('on-page-change')
+        if (this.options.url) {
+          this._reload()
         }
-    }
+      },
 
-    function mockData() {
-        return {
-            options: {
-                tableColumns: [
-                    {
-                        title: '姓名',
-                        key: 'name'
-                    },
-                    {
-                        title: '年龄',
-                        key: 'age'
-                    },
-                    {
-                        title: '地址',
-                        key: 'address'
-                    }
-                ],
-                tableData: [
-                    {
-                        name: '王小明',
-                        age: 18,
-                        address: '北京市朝阳区芍药居'
-                    },
-                    {
-                        name: '张小刚',
-                        age: 25,
-                        address: '北京市海淀区西二旗'
-                    },
-                    {
-                        name: '李小红',
-                        age: 30,
-                        address: '上海市浦东新区世纪大道'
-                    },
-                    {
-                        name: '周小伟',
-                        age: 26,
-                        address: '深圳市南山区深南大道'
-                    }
-                ]
-            }
+      onPageSizeChange(pageSize){
+        this.$emit('on-page-size-change')
+        this.pageSize = pageSize
+        if (this.options.url) {
+          this._reload()
         }
+      },
+
+      rowSelect(data){
+        this.$emit('on-select', data)
+      },
+
+      selectAll(){
+        this.$emit('on-selection-change', ...arguments)
+      }
     }
+  }
+
+  function mockData() {
+    return {
+      options: {
+        tableColumns: [
+          {
+            title: '姓名',
+            key: 'name'
+          },
+          {
+            title: '年龄',
+            key: 'age'
+          },
+          {
+            title: '地址',
+            key: 'address'
+          }
+        ],
+        tableData: [
+          {
+            name: '王小明',
+            age: 18,
+            address: '北京市朝阳区芍药居'
+          },
+          {
+            name: '张小刚',
+            age: 25,
+            address: '北京市海淀区西二旗'
+          },
+          {
+            name: '李小红',
+            age: 30,
+            address: '上海市浦东新区世纪大道'
+          },
+          {
+            name: '周小伟',
+            age: 26,
+            address: '深圳市南山区深南大道'
+          }
+        ]
+      }
+    }
+  }
 </script>
